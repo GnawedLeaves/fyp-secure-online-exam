f68c1fd1446a937ad7a45c449482a58cbca99114
Marcel <Marcelyap31@gmail.com>
2023-11-08 14:03:45 +0800

    base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import StudentHomepage from "./pages/student/StudentHomepage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<StudentHomepage />} />
+          <Route path="/studentHome" element={<StudentHomepage />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/pages/student/StudentHomepage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentHomepage = () => {
+  return <div>hi</div>;
+};
+
+export default StudentHomepage;
