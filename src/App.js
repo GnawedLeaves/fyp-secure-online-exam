36e2c2c2666348532c831a19e2c734afc734fb10
Marcel <Marcelyap31@gmail.com>
2024-01-17 14:13:40 +0800

    updated app,js with master and updated stylings

--- src/App.js
+++ src/App.js
@@ -10,37 +10,35 @@ import StudentExamQuestionPage from "./pages/student/StudentExamQuestionpage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
 import Loginpage from "./pages/common/LoginPage";
-import AdminHomePage from "./pages/admin/AdminHomePage";
-import Navbar from "./components/Navbar/Navbar";
-import AdminExamDetailsPage from "./pages/admin/AdminExamDetailsPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense>
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path="/" element={<Loginpage />} />
-          <Route path="/student/dashboard" element={<StudentHomepage />} />
-          <Route
-            path="/student/systemcheck"
-            element={<StudentSystemCheckPage />}
-          />
-          <Route path="/student/exam" element={<StudentExamPage />} />
-          <Route
-            path="/student/bug_report"
-            element={<StudentBugReportPage />}
-          />
-          <Route
-            path="/student/exam/exam_detail"
-            element={<StudentExamDetailPage />}
-          />
-          <Route
-            path="/student/exam/question"
-            element={<StudentExamQuestionPage />}
-          />
-        </Routes>
-      </Suspense>
+      <StudentNavbar>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<Loginpage />} />
+            <Route path="/student/dashboard" element={<StudentHomepage />} />
+            <Route
+              path="/student/systemcheck"
+              element={<StudentSystemCheckPage />}
+            />
+            <Route path="/student/exam" element={<StudentExamPage />} />
+            <Route
+              path="/student/bug_report"
+              element={<StudentBugReportPage />}
+            />
+            <Route
+              path="/student/exam/exam_detail"
+              element={<StudentExamDetailPage />}
+            />
+            <Route
+              path="/student/exam/question"
+              element={<StudentExamQuestionPage />}
+            />
+          </Routes>
+        </Suspense>
+      </StudentNavbar>
     </BrowserRouter>
   );
 }
