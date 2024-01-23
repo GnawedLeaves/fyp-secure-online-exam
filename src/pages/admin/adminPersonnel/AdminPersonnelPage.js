185e196219e53e46fe695440d135ff71a672b2dc
Marcel <Marcelyap31@gmail.com>
2024-01-23 16:01:40 +0800

    Added toggle button and filter buttons bar

--- src/pages/admin/adminPersonnel/AdminPersonnelPage.js
+++ src/pages/admin/adminPersonnel/AdminPersonnelPage.js
@@ -3,16 +3,40 @@ import { theme } from "../../../theme";
 import {
   AdminPersonnelBigContainer,
   AdminPersonnelContainer,
+  AdminPersonnelFiltersContainer,
+  AdminPersonnelFiltersTitle,
+  AdminPersonnelNavbarContainer,
 } from "./AdminPersonnelStyles";
 import Navbar from "../../../components/Navbar/Navbar";
 import { adminNavbarItems } from "../AdminHomePage";
+import Button from "../../../components/Button/Button";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import FilterBar from "../../../components/FilterBar/FilterBar";
 
 const AdminPersonnelPage = () => {
+  const filters = ["Teacher", "Student", "Admin", "Others"];
+  const [filtersSelected, setFiltersSelected] = useState(["All"]);
+
+  const handleFilterBarData = (data) => {
+    setFiltersSelected(data);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <AdminPersonnelBigContainer>
         <Navbar linksArray={adminNavbarItems} />
-        <AdminPersonnelContainer>Admin Personnel Page</AdminPersonnelContainer>
+        <AdminPersonnelContainer>
+          <AdminPersonnelNavbarContainer>
+            <FilterBar
+              filters={filters}
+              handleFilterBarData={handleFilterBarData}
+              buttonFilledColor={theme.primary}
+            />
+            <Button>Add New +</Button>
+          </AdminPersonnelNavbarContainer>
+          {filtersSelected}
+        </AdminPersonnelContainer>
       </AdminPersonnelBigContainer>
     </ThemeProvider>
   );
--- src/pages/admin/adminPersonnel/AdminPersonnelStyles.js
+++ src/pages/admin/adminPersonnel/AdminPersonnelStyles.js
@@ -5,4 +5,23 @@ export const AdminPersonnelBigContainer = styled.div`
 `;
 export const AdminPersonnelContainer = styled.div`
   padding: 2rem;
+  width: 100%;
+`;
+
+export const AdminPersonnelFiltersContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+`;
+
+export const AdminPersonnelFiltersTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: 600;
+`;
+
+export const AdminPersonnelNavbarContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
 `;
