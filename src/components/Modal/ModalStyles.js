2a953153196619eecad4f8ec083de69130d529b4
Marcel <Marcelyap31@gmail.com>
2024-01-27 20:16:18 +0800

    modal completed

--- src/components/Modal/ModalStyles.js
+++ src/components/Modal/ModalStyles.js
@@ -2,21 +2,28 @@ import styled from "styled-components";
 
 export const ModalContainer = styled.dialog`
   border-radius: 1rem;
-  // min-width: 15rem;
-  min-height: 15rem;
+  width: 23rem;
+  min-height: 10rem;
   border: none;
   display: ${(props) => (props.display ? "flex" : "none")};
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 0.8rem;
+  gap: 0.9rem;
+  padding: 2rem;
 `;
 
 export const ModalTitle = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.6rem;
   font-weight: bold;
 `;
 export const ModalContent = styled.div`
-  width: 70%;
+  width: 90%;
   text-align: center;
+`;
+
+export const ModalButtonContainer = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  gap: 1rem;
 `;
