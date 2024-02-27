7ac67cd6d856429c3fa9ad5a04a4575f108207f6
Marcel <Marcelyap31@gmail.com>
2024-02-27 19:12:47 +0800

    Added personnel and auth

--- src/backend/firebase/firebase.js
+++ src/backend/firebase/firebase.js
@@ -14,16 +14,16 @@ const firebaseConfig = {
   //authDomain: "online-exam-platform-97200.firebaseapp.com",
   //projectId: "online-exam-platform-97200",
   //storageBucket: "online-exam-platform-97200.appspot.com",
-  messagingSenderId: "192481901185",
   //appId: "1:192481901185:web:e32fff03d5c905832d2b80",
   //measurementId: "G-Y6Q02KHVXS",
 
-  apiKey: "AIzaSyA8iEAT9lc9dhsZuVaAR01FXtwkk1v6XrE",
-  authDomain: "temporaryfyp-21230.firebaseapp.com",
-  projectId: "temporaryfyp-21230",
-  storageBucket: "temporaryfyp-21230.appspot.com",
-  messagingSenderId: "189128448631",
-  appId: "1:189128448631:web:2ae5974fa64bcd6262c472"
+  apiKey: "AIzaSyAnM3OR0JNYbhpGRLd5fmxhAlEwhKNmf8o",
+  authDomain: "online-exam-platform-97200.firebaseapp.com",
+  projectId: "online-exam-platform-97200",
+  storageBucket: "online-exam-platform-97200.appspot.com",
+  messagingSenderId: "192481901185",
+  appId: "1:192481901185:web:e32fff03d5c905832d2b80",
+  measurementId: "G-Y6Q02KHVXS",
 };
 
 // Initialize Firebase
