23438728461686727c79cae7b860425dd894f4f3
Marcel <Marcelyap31@gmail.com>
2024-03-04 16:35:45 +0800

    started on bubble select

---
+++ src/components/BubbleSelect/BubbleSelectStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BubbleSelectContainer = styled.div`
+  border: 1px solid red;
+`;
+
+export const Bubble = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const BubbleContainer = styled.div``;
