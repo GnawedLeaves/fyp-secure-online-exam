a87594490b2ad17aaa7ce204d2527a86c79e00db
CheongCKai <113497072+CheongCKai@users.noreply.github.com>
2024-03-05 20:17:56 +0800

    Add files via upload

---
+++ src/components/Modal/InstructorModal.js
@@ -0,0 +1,222 @@
+import React from "react";
+import { useRef } from "react";
+import { useState } from 'react';
+import { useEffect } from "react";
+import {
+  EmptyModalCloseContainer,
+  EmptyModalContainer,
+  ModalButtonContainer,
+  ModalContainer,
+  ModalContent,
+  ModalSucess,
+  ModalTitle,
+  ModalTitleContainer,
+  ModalExamContainer,
+} from "./InstructorModalStyles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../theme";
+import Button from "../Button/Button";
+import { IoMdClose } from "react-icons/io";
+
+//Props:
+{
+  /* <Modal
+handleModalClose={() => {
+  setShowModal(false);
+}}
+modalType="action"
+actionButtonText="Delete"
+actionButtonColor={theme.statusError}
+actionButtonClick={() => {}}
+show={showModal}
+modalTitle="Delete User"
+modalContent="Are you sure you want to delete this user? This action cannot be undone."
+/> */
+}
+
+//Exmaple of empty modal with custom content:
+{
+  /* <Modal
+  show={showModal}
+  handleModalClose={() => {
+    setShowModal(false);
+  }}
+  modalType="empty"
+  showCross={true}
+>
+  <div style={{ width: "20rem", background: "red", height: "20rem" }}>
+    content goes here
+  </div>
+</Modal>; */
+}
+
+//Usage:
+//1. In the parent element just send a boolean prop (true/false), true will open the modal, false will close it.
+
+const InstructorModal = (props) => {
+  const modalRef = useRef(null);
+  const modalType = props.modalType;
+
+  const showModal = () => {
+    if (modalRef.current) {
+      modalRef.current.showModal();
+    }
+  };
+
+  const closeModal = () => {
+    if (modalRef.current) {
+      modalRef.current.close();
+      props.handleModalClose();
+    }
+  };
+
+  useEffect(() => {
+    if (props.show) {
+      showModal();
+    } else {
+      closeModal();
+    }
+  }, [props.show]);
+
+  const handleOkClick = () => {
+    closeModal();
+    props.actionButtonClick();
+  };
+
+  const getModalType = () => {
+    switch (modalType) {
+      case "empty":
+        return (
+          <EmptyModalContainer ref={modalRef} display={props.show}>
+            <EmptyModalCloseContainer display={props.showCross}>
+              <IoMdClose
+                size="1.8rem"
+                onClick={() => {
+                  closeModal();
+                }}
+                style={{ cursor: "pointer" }}
+              />
+            </EmptyModalCloseContainer>
+
+            {props.children}
+          </EmptyModalContainer>
+        );
+      case "action":
+        return (
+          <ModalContainer display={props.show} ref={modalRef}>
+            <ModalTitle>{props.modalTitle}</ModalTitle>
+            <ModalContent>{props.modalContent}</ModalContent>
+            <ModalButtonContainer>
+              <Button
+                filled={true}
+                filledColor={props.actionButtonColor}
+                defaultColor={props.actionButtonColor}
+                onClick={() => {
+                  closeModal();
+                  props.actionButtonClick();
+                }}
+              >
+                {props.actionButtonText ? props.actionButtonText : "OK"}
+              </Button>
+              <Button
+                onClick={() => {
+                  closeModal();
+                }}
+              >
+                {props.closingButtonText ? props.closingButtonText : "Cancel"}
+              </Button>
+            </ModalButtonContainer>
+          </ModalContainer>
+        );
+
+
+
+        case "showQuestions":
+        return (
+          <div>
+            <ModalContainer display={props.show} ref={modalRef}>
+            <ModalTitle>{props.modalTitle}</ModalTitle>
+            <ModalExamContainer>
+            {Array.isArray(props.modalContent) && props.modalContent.length > 0 ? (
+              props.modalContent.map((question, index) => (
+                <div key={index}>
+                   <ModalContent>{props.modalContent}</ModalContent>
+                </div>
+              ))
+            ) : (
+              <ModalContent>No questions found for this exam.</ModalContent>
+            )}
+          </ModalExamContainer>
+            <ModalButtonContainer>
+              <Button
+                filled={true}
+                filledColor={props.actionButtonColor}
+                defaultColor={props.actionButtonColor}
+                onClick={handleOkClick}
+              >
+                {props.actionButtonText ? props.actionButtonText : "OK"}
+              </Button>
+              <Button onClick={closeModal}>
+                {props.closingButtonText ? props.closingButtonText : "Cancel"}
+              </Button>
+            </ModalButtonContainer>
+            </ModalContainer>
+          </div>
+        );
+
+
+        case "successExam":
+        return (
+          <ModalContainer display={props.show} ref={modalRef}>
+            <ModalSucess>{props.modalTitle}</ModalSucess>
+            <ModalContent>{props.modalContent}</ModalContent>
+            <ModalButtonContainer>
+              <Button
+                filled={true}
+                filledColor={props.actionButtonColor}
+                defaultColor={props.actionButtonColor}
+                onClick={() => {
+                  closeModal();
+                  props.actionButtonClick();
+                }}
+              >
+                {props.actionButtonText ? props.actionButtonText : "OK"}
+              </Button>
+            </ModalButtonContainer>
+          </ModalContainer>
+        );
+
+      default:
+        return (
+          <ModalContainer display={props.show} ref={modalRef}>
+            <ModalTitle>{props.modalTitle}</ModalTitle>
+            <ModalContent>{props.modalContent}</ModalContent>
+            <ModalButtonContainer>
+                <Button
+                filled={true}
+                filledColor={props.actionButtonColor}
+                defaultColor={props.actionButtonColor}
+                onClick={() => {
+                  closeModal();
+                  props.actionButtonClick();
+                }}
+              >
+                {props.actionButtonText ? props.actionButtonText : "OK"}
+              </Button>
+              <Button
+                onClick={() => {
+                  closeModal();
+                }}
+              >
+                {props.closingButtonText ? props.closingButtonText : "Cancel"}
+              </Button>
+            </ModalButtonContainer>
+          </ModalContainer>
+        );
+    }
+  };
+
+  return <ThemeProvider theme={theme}>{getModalType()}</ThemeProvider>;
+};
+
+export default InstructorModal;
---
+++ src/components/Modal/InstructorModalStyles.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+// export const ModalContainer = styled.dialog`
+//   border-radius: 1rem;
+//   width: 35rem;
+//   min-height: 15rem;
+//   border: none;
+//   display: ${(props) => (props.display ? "flex" : "none")};
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 0.9rem;
+//   padding: 2rem;
+// `;
+export const ModalContainer = styled.dialog`
+  border-radius: 1rem;
+  width: 35rem;
+  max-height: 80vh;
+  border: none;
+  overflow-y: auto; /* Enable vertical scrolling if content exceeds modal height */
+  display: ${(props) => (props.display ? "flex" : "none")};
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.9rem;
+  padding: 2rem;
+`;
+
+export const ModalExamContainer = styled.dialog`
+max-height: calc(100vh - 200px);
+overflow-y: auto; 
+`;
+
+export const ModalTitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const ModalTitle = styled.div`
+  font-size: 1.6rem;
+  font-weight: bold;
+
+`;
+export const ModalSucess = styled.div`
+  font-size: 2.6rem;
+  font-weight: bold;
+
+`;
+
+export const ModalContent = styled.div`
+  width: 90%;
+  // text-align: center;
+`;
+
+export const ModalButtonContainer = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+`;
+
+export const EmptyModalContainer = styled.dialog`
+  display: ${(props) => (props.display ? "" : "none")};
+  border-radius: 1rem;
+  border: none;
+  min-height: 10rem;
+  min-width: 20rem;
+  padding: 1rem;
+`;
+export const EmptyModalCloseContainer = styled.div`
+  width: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  display: ${(props) => (props.display ? "flex" : "none")};
+`;
