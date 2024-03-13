3822515406d9266061c24f248c7c9c0a206f9883
CheongCKai <113497072+CheongCKai@users.noreply.github.com>
2024-03-14 00:19:11 +0800

    exam Calendar in sync with database

--- src/pages/Instructor/CalendarGenerator.js
+++ src/pages/Instructor/CalendarGenerator.js
@@ -30,6 +30,29 @@ const buttonStyle = {
 
 const ExamCalendar = () => {
   const [date, setDate] = useState(new Date());
+  const [examDates, setExamDates] = useState([]);
+
+  const fetchExams = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'exams'));
+      const dates = [];
+
+      querySnapshot.forEach((doc) => {
+        const examData = doc.data();
+        const startTime = new Date(examData.startTime.seconds * 1000); // Convert Firestore Timestamp to JavaScript Date object
+        dates.push(new Date(startTime.getFullYear(), startTime.getMonth(), startTime.getDate())); // Extract date portion only
+      });
+
+      setExamDates(dates);
+      console.log("Fetched exam dates:", dates); 
+    } catch (error) {
+      console.error("Error fetching exams:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchExams();
+  }, []);
 
   const daysInMonth = (month, year) => {
     return new Date(year, month + 1, 0).getDate();
@@ -49,7 +72,41 @@ const ExamCalendar = () => {
         if ((i === 0 && j < firstDay) || day > days) {
           week.push(null);
         } else {
-          week.push(day);
+          const currentDate = new Date(year, month, day);
+          const isToday = isSameDate(currentDate, new Date());
+          const isExamDay = examDates.some(examDate => isSameDate(examDate, currentDate));
+          const isYesterday = isSameDate(currentDate, new Date(Date.now() - 86400000)); // 86400000 ms in a day
+          const isBeforeToday = currentDate < new Date();
+          const isExamBeforeToday = isBeforeToday && isExamDay && !isToday;
+          const isWeekend = j === 0 || j === 6;
+  
+          let backgroundColor = "";
+          if (isExamDay) {
+            if (isToday) {
+              backgroundColor = "red"; // Exam today
+            } else if (isYesterday || isBeforeToday) {
+              backgroundColor = "#E3E1D9"; // Exam yesterday or before today
+            } else {
+              backgroundColor = "#FFA447"; // Exam not today and not yesterday, but after yesterday
+            }
+          } else {
+            if (isToday) {
+              backgroundColor = "lightgreen"; // Current day with no exam
+            } else {
+              backgroundColor = null; // Regular day with no exam
+            }
+          }
+  
+          week.push({
+            day: day,
+            isToday: isToday,
+            isExamDay: isExamDay,
+            isYesterday: isYesterday,
+            isBeforeToday: isBeforeToday,
+            isExamBeforeToday: isExamBeforeToday,
+            isWeekend: isWeekend,
+            backgroundColor: backgroundColor
+          });
           day++;
         }
       }
@@ -57,6 +114,12 @@ const ExamCalendar = () => {
       if (day > days) break;
     }
     return monthData;
+  };
+
+  const isSameDate = (date1, date2) => {
+    return date1.getDate() === date2.getDate() &&
+           date1.getMonth() === date2.getMonth() &&
+           date1.getFullYear() === date2.getFullYear();
   };
 
   const prevMonth = () => {
@@ -85,69 +148,74 @@ const ExamCalendar = () => {
   const monthData = getMonthData();
   const today = new Date();
 
+  
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <div style={{ width: "100%", textAlign: "center", paddingTop: "2%", padding: "10px", border: "1px solid #ccc", borderRadius: "5px", margin: "20px 0", boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)" }}>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <button onClick={prevMonth} style={buttonStyle}>Prev</button>
-          <span style={{ fontSize: "35px", fontWeight: "bold", margin: "0 10px" }}>
-            {monthNames[date.getMonth()]} {date.getFullYear()}
-          </span>
-          <button onClick={nextMonth} style={buttonStyle}>Next</button>
-        </div>
-        <table style={{ width: "100%" }}>
-          <thead>
-            <tr>
-              <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Sun</th>
-              <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Mon</th>
-              <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Tue</th>
-              <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Wed</th>
-              <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Thu</th>
-              <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Fri</th>
-              <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Sat</th>
-            </tr>
-          </thead>
-          <tbody>
-            {monthData.map((week, index) => (
-              <tr key={index} style={{ border: "1px solid #ccc" }}>
-                {week.map((day, index) => (
-                  <td
-                    key={index}
-                    style={{
-                      border: "1px solid #ccc",
-                      fontSize: "25px",
-                      padding: "10px",
-                      backgroundColor: (day === today.getDate() && date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear()) ? "lightgreen" : (index === 0 || index === 6) ? "#e0e0e0" : "inherit", // Light green for today's date, grey for weekends
-                      color: (index === 0 || index === 6) ? "#666" : "inherit", // Change text color for weekends
-                      fontWeight: (day === today.getDate() && date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear()) ? "bold" : "normal" // Bold font for today's date
-                    }}
-                  >
-                    {day}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div style={{ width: "100%", textAlign: "center", paddingTop: "2%", padding: "10px", border: "1px solid #ccc", borderRadius: "5px", margin: "20px 0", boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)" }}>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <button onClick={prevMonth} style={buttonStyle}>Prev</button>
+        <span style={{ fontSize: "35px", fontWeight: "bold", margin: "0 10px" }}>
+          {monthNames[date.getMonth()]} {date.getFullYear()}
+        </span>
+        <button onClick={nextMonth} style={buttonStyle}>Next</button>
       </div>
-      <div style={{ width: "100%", textAlign: "center", paddingBottom: "20px" }}>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <div style={{ display: "inline-block", marginRight: "20px" }}>
-            <span style={{ display: "inline-block", width: "20px", height: "20px", backgroundColor: "lightgreen", marginRight: "5px" }}></span>
-            <span style={{ color: "black" }}>Today</span>
-          </div>
-          <div style={{ display: "inline-block", marginRight: "20px" }}>
-            <span style={{ display: "inline-block", width: "20px", height: "20px", backgroundColor: "orange", marginRight: "5px" }}></span>
-            <span style={{ color: "black" }}>Exam Days</span>
-          </div>
-          <div style={{ display: "inline-block" }}>
-            <span style={{ display: "inline-block", width: "20px", height: "20px", backgroundColor: "red", marginRight: "5px" }}></span>
-            <span style={{ color: "black" }}>Exam Today</span>
-          </div>
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Sun</th>
+            <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Mon</th>
+            <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Tue</th>
+            <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Wed</th>
+            <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Thu</th>
+            <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Fri</th>
+            <th style={{ border: "1px solid #ccc", fontSize: "27px", padding: "10px" }}>Sat</th>
+          </tr>
+        </thead>
+        <tbody>
+          {monthData.map((week, index) => (
+            <tr key={index} style={{ border: "1px solid #ccc" }}>
+              {week.map((day, index) => (
+                 <td
+                 key={index}
+                 style={{
+                   border: "1px solid #ccc",
+                   fontSize: "25px",
+                   padding: "10px",
+                   backgroundColor: day && (day.isToday || day.isExamDay) ? day.backgroundColor : (day && day.isWeekend) ? "#B4B4B8" : null,
+                   color: (index === 0 || index === 6) ? "#666" : "inherit",
+                   fontWeight: day && day.isToday ? "bold" : "normal"
+                 }}
+               >
+                  {day ? day.day : null}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <div style={{ width: "100%", textAlign: "center", paddingBottom: "20px" }}>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div style={{ display: "inline-block", marginRight: "20px" }}>
+          <span style={{ display: "inline-block", width: "20px", height: "20px", backgroundColor: "##E3E1D9", marginRight: "5px" }}></span>
+          <span style={{ color: "black" }}>Past Exams</span>
+        </div>
+        <div style={{ display: "inline-block", marginRight: "20px" }}>
+          <span style={{ display: "inline-block", width: "20px", height: "20px", backgroundColor: "lightgreen", marginRight: "5px" }}></span>
+          <span style={{ color: "black" }}>Today</span>
+        </div>
+        <div style={{ display: "inline-block", marginRight: "20px" }}>
+          <span style={{ display: "inline-block", width: "20px", height: "20px", backgroundColor: "#FFA447", marginRight: "5px" }}></span>
+          <span style={{ color: "black" }}>Exam Days</span>
+        </div>
+        <div style={{ display: "inline-block" }}>
+          <span style={{ display: "inline-block", width: "20px", height: "20px", backgroundColor: "red", marginRight: "5px" }}></span>
+          <span style={{ color: "black" }}>Exam Today</span>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default ExamCalendar;
