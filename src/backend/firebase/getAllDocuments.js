8d81b086b677a72e0dd5611d8ac6335031a50df5
Marcel <Marcelyap31@gmail.com>
2024-04-12 00:32:28 +0800

    init commit

--- src/backend/firebase/getAllDocuments.js
+++ src/backend/firebase/getAllDocuments.js
@@ -32,3 +32,20 @@ export const getAllDocuments = async (collectionName) => {
     console.log("error: ", e);
   }
 };
+
+export const getAllDocumentsWithoutDate = async (collectionName) => {
+  try {
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    if (querySnapshot.docs) {
+      const allDocumentData = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        console.log(data);
+        return { id: doc.id, ...data };
+      });
+
+      return allDocumentData;
+    }
+  } catch (e) {
+    console.log("error: ", e);
+  }
+};
