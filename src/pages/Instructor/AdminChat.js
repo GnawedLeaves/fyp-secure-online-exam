73fd3f6c2b00e398bf4256ebe268b26e5ca14d94
Limpei <million4you@hotmail.com>
2024-04-17 14:22:42 +0800

    chatbox

--- src/pages/Instructor/AdminChat.js
+++ src/pages/Instructor/AdminChat.js
@@ -46,169 +46,171 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import ChatboxBig from "../../../src/components/Chatbox/ChatboxBig";
 
 const AdminChat = () => {
-  const messagesRef = collection(db, "messages");
-  const [userId, setUserId] = useState();
-  const [allMessagesData, setAllMessagesData] = useState([]);
-  const [uniqueSenderIds, setUniqueSenderIds] = useState([]);
-  const [uniquePreviewChats, setUniquePreviewChats] = useState([]);
-  const [chatIdSelected, setChatIdSelected] = useState("");
-  const [chatNameSelected, setChatNameSelected] =
-    useState("Name Not Available");
-
-  const auth = getAuth();
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const uid = user.uid;
-        getUserId(uid);
+    const messagesRef = collection(db, "messages");
+    const [userId, setUserId] = useState();
+    const [allMessagesData, setAllMessagesData] = useState([]);
+    const [uniqueSenderIds, setUniqueSenderIds] = useState([]);
+    const [uniquePreviewChats, setUniquePreviewChats] = useState([]);
+    const [chatIdSelected, setChatIdSelected] = useState("");
+    const [chatNameSelected, setChatNameSelected] =
+      useState("Name Not Available");
+  
+    const auth = getAuth();
+    useEffect(() => {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+          getUserId(uid);
+        }
+      });
+    }, []);
+  
+    const getUserId = async (authId) => {
+      const q = query(collection(db, "users"), where("authId", "==", authId));
+      const querySnapshot = await getDocs(q);
+      if (!querySnapshot.empty) {
+        const firstDoc = querySnapshot.docs[0];
+        const userId = firstDoc.id;
+        setUserId(userId);
       }
-    });
-  }, []);
-
-  const getUserId = async (authId) => {
-    const q = query(collection(db, "users"), where("authId", "==", authId));
-    const querySnapshot = await getDocs(q);
-    if (!querySnapshot.empty) {
-      const firstDoc = querySnapshot.docs[0];
-      const userId = firstDoc.id;
-      setUserId(userId);
-    }
-  };
-
-  useEffect(() => {
-    getAllMessages();
-  }, [userId]);
-
-  useEffect(() => {
-    constructPreviews();
-  }, [uniqueSenderIds]);
-
-  //Unqiue senders ids
-  useEffect(() => {
-    const filteredSenderIDs = allMessagesData
-      .filter((message) => message.recipientId === userId)
-      .map((message) => message.senderId);
-    const uniqueArray = [...new Set(filteredSenderIDs)];
-    setUniqueSenderIds(uniqueArray);
-  }, [allMessagesData]);
-
-  const constructPreviews = async () => {
-    try {
-      // Filter messages only once
-      const filteredMessages = uniqueSenderIds.map((senderId) =>
-        allMessagesData.find((message) => message.senderId === senderId)
-      );
-
-      // Fetch user names for all filtered messages in parallel
-      const userNamesPromises = filteredMessages.map((message) =>
-        getUserNameFromId(message.senderId)
-      );
-      const userNames = await Promise.all(userNamesPromises);
-
-      // Combine filtered messages with user names
-      const filteredMessagesWithName = filteredMessages.map(
-        (message, index) => ({
-          ...message,
-          name: userNames[index], // Use the corresponding user name
-        })
-      );
-
-      // Update state with filtered messages including user names
-
-      setUniquePreviewChats(filteredMessagesWithName);
-    } catch (error) {
-      console.error("Error constructing previews:", error);
-    }
-  };
-
-  const getAllMessages = async (recipientId, senderId) => {
-    const recivedMessages = await getRecievedMessages(userId);
-    const sentMessages = await getSentMessages(userId);
-    //const sentMessages = await getSentMessages(recipientId, senderId);
-    const allMessages = [...recivedMessages, ...sentMessages];
-    const sortedArray = sortByFirebaseTimestamp(
-      allMessages,
-      "dateAdded"
-    ).reverse();
-    setAllMessagesData([...sortedArray]);
-  };
-
-  //Get messages from database and display them
-  const getRecievedMessages = async (recipientId) => {
-    try {
-      // Create a query to get all messages where recipientId matches
-      const messagesQuery = query(
-        messagesRef,
-        where("recipientId", "==", recipientId)
-      );
-
-      // Get the documents based on the query
-      const querySnapshot = await getDocs(messagesQuery);
-
-      // Extract the data from the documents
-      const messages = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      return messages;
-    } catch (error) {
-      console.error("Error getting messages:", error);
-      return [];
-    }
-  };
-
-  const getSentMessages = async (senderId) => {
-    try {
-      // Create a query to get all messages where recipientId matches
-      const messagesQuery = query(
-        messagesRef,
-        where("senderId", "==", senderId)
-      );
-
-      // Get the documents based on the query
-      const querySnapshot = await getDocs(messagesQuery);
-
-      // Extract the data from the documents
-      const messages = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      return messages;
-    } catch (error) {
-      console.error("Error getting messages:", error);
-      return [];
-    }
-  };
-
-  const constructTruncatedText = (text, maxLength) => {
-    const truncatedText =
-      text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
-    return truncatedText;
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Escape") {
-      setChatIdSelected("");
-    }
-  };
-
-  const getUserNameFromId = async (id) => {
-    const userRef = doc(db, "users", id);
-
-    try {
-      const docSnap = await getDoc(userRef);
-      if (docSnap.exists()) {
-        const userData = docSnap.data();
-
-        return userData.name + " (" + userData.type + " ID: " + id + ")";
-      } else {
-        return "Name Not Available (ID: " + id + ")";
+    };
+  
+    useEffect(() => {
+      getAllMessages();
+    }, [userId]);
+  
+    useEffect(() => {
+      constructPreviews();
+    }, [uniqueSenderIds]);
+  
+    //Unqiue senders ids
+    useEffect(() => {
+      const filteredSenderIDs = allMessagesData
+        .filter((message) => message.recipientId === userId)
+        .map((message) => message.senderId);
+      const uniqueArray = [...new Set(filteredSenderIDs)];
+      setUniqueSenderIds(uniqueArray);
+    }, [allMessagesData]);
+  
+    //function to get all messages with the specific userId
+    const constructPreviews = async () => {
+      try {
+        // Filter messages only once
+        const filteredMessages = uniqueSenderIds.map((senderId) =>
+          allMessagesData.find((message) => message.senderId === senderId)
+        );
+  
+        // Fetch user names for all filtered messages in parallel
+        const userNamesPromises = filteredMessages.map((message) =>
+          getUserNameFromId(message.senderId)
+        );
+        const userNames = await Promise.all(userNamesPromises);
+  
+        // Combine filtered messages with user names
+        const filteredMessagesWithName = filteredMessages.map(
+          (message, index) => ({
+            ...message,
+            name: userNames[index], // Use the corresponding user name
+          })
+        );
+  
+        // Update state with filtered messages including user names
+  
+        setUniquePreviewChats(filteredMessagesWithName);
+      } catch (error) {
+        console.error("Error constructing previews:", error);
       }
-    } catch (e) {
-      console.log("Error getting user data", e);
-    }
-  };
+    };
+  
+    const getAllMessages = async (recipientId, senderId) => {
+      const recivedMessages = await getRecievedMessages(userId);
+      const sentMessages = await getSentMessages(userId);
+      //const sentMessages = await getSentMessages(recipientId, senderId);
+      const allMessages = [...recivedMessages, ...sentMessages];
+      const sortedArray = sortByFirebaseTimestamp(
+        allMessages,
+        "dateAdded"
+      ).reverse();
+      setAllMessagesData([...sortedArray]);
+    };
+  
+    //Get messages from database and display them
+    const getRecievedMessages = async (recipientId) => {
+      try {
+        // Create a query to get all messages where recipientId matches
+        const messagesQuery = query(
+          messagesRef,
+          where("recipientId", "==", recipientId)
+        );
+  
+        // Get the documents based on the query
+        const querySnapshot = await getDocs(messagesQuery);
+  
+        // Extract the data from the documents
+        const messages = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        return messages;
+      } catch (error) {
+        console.error("Error getting messages:", error);
+        return [];
+      }
+    };
+  
+    const getSentMessages = async (senderId) => {
+      try {
+        // Create a query to get all messages where recipientId matches
+        const messagesQuery = query(
+          messagesRef,
+          where("senderId", "==", senderId)
+        );
+  
+        // Get the documents based on the query
+        const querySnapshot = await getDocs(messagesQuery);
+  
+        // Extract the data from the documents
+        const messages = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+  
+        return messages;
+      } catch (error) {
+        console.error("Error getting messages:", error);
+        return [];
+      }
+    };
+  
+    const constructTruncatedText = (text, maxLength) => {
+      const truncatedText =
+        text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+      return truncatedText;
+    };
+  
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        setChatIdSelected("");
+      }
+    };
+  
+    const getUserNameFromId = async (id) => {
+      const userRef = doc(db, "users", id);
+  
+      try {
+        const docSnap = await getDoc(userRef);
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+  
+          return userData.name + " (" + userData.type + " ID: " + id + ")";
+        } else {
+          return "Name Not Available (ID: " + id + ")";
+        }
+      } catch (e) {
+        console.log("Error getting user data", e);
+      }
+    };
+  
 
   return (
     
