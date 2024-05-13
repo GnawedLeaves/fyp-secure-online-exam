0c73663fe9a02177a60a9a2fab6adae2b867e4b1
Limpei <million4you@hotmail.com>
2024-05-14 00:58:29 +0800

    update

--- src/backend/firebase/firebase.js
+++ src/backend/firebase/firebase.js
@@ -10,20 +10,21 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnM3OR0JNYbhpGRLd5fmxhAlEwhKNmf8o",
-  authDomain: "online-exam-platform-97200.firebaseapp.com",
-  projectId: "online-exam-platform-97200",
-  storageBucket: "online-exam-platform-97200.appspot.com",
-  messagingSenderId: "192481901185",
-  appId: "1:192481901185:web:e32fff03d5c905832d2b80",
-  measurementId: "G-Y6Q02KHVXS",
+  // apiKey: "AIzaSyAnM3OR0JNYbhpGRLd5fmxhAlEwhKNmf8o",
+  // authDomain: "online-exam-platform-97200.firebaseapp.com",
+  // projectId: "online-exam-platform-97200",
+  // storageBucket: "online-exam-platform-97200.appspot.com",
+  // messagingSenderId: "192481901185",
+  // appId: "1:192481901185:web:e32fff03d5c905832d2b80",
+  // measurementId: "G-Y6Q02KHVXS",
 
-  //apiKey: "AIzaSyA8iEAT9lc9dhsZuVaAR01FXtwkk1v6XrE",
-  //authDomain: "temporaryfyp-21230.firebaseapp.com",
-  //projectId: "temporaryfyp-21230",
-  //storageBucket: "temporaryfyp-21230.appspot.com",
-  //messagingSenderId: "189128448631",
-  //appId: "1:189128448631:web:2ae5974fa64bcd6262c472"
+  apiKey: "AIzaSyCSAPU4LlT_pQxWShbGmh5r2PuUUvzQOBI",
+  authDomain: "fyp-sample-3f6cc.firebaseapp.com",
+  projectId: "fyp-sample-3f6cc",
+  storageBucket: "fyp-sample-3f6cc.appspot.com",
+  messagingSenderId: "483265173025",
+  appId: "1:483265173025:web:ef8f538fb51867421af560",
+  measurementId: "G-GM4P6FNQ7B"
 };
 
 // Initialize Firebase
--- src/pages/Instructor/CalendarGenerator.js
+++ src/pages/Instructor/CalendarGenerator.js
@@ -32,20 +32,30 @@ const buttonStyle = {
 const ExamCalendar = () => {
   const [date, setDate] = useState(new Date());
   const [examDates, setExamDates] = useState([]);
+  const [examNames, setExamNames] = useState([]);
+  const [examTimes, setExamTimes] = useState([]);
   
   const fetchExams = async () => {
     try {
       const querySnapshot = await getDocs(collection(db, 'exams'));
       const dates = [];
+      const names = [];
+      const times = [];
 
       querySnapshot.forEach((doc) => {
         const examData = doc.data();
         const startTime = new Date(examData.startTime.seconds * 1000); // Convert Firestore Timestamp to JavaScript Date object
         dates.push(new Date(startTime.getFullYear(), startTime.getMonth(), startTime.getDate())); // Extract date portion only
+        names.push(examData.name);
+        times.push(startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
       });
 
       setExamDates(dates);
+      setExamNames(names);
+      setExamTimes(times);
       console.log("Fetched exam dates:", dates); 
+      console.log("Fetched exam names:", names);
+      console.log("Fetched exam names:", times);
     } catch (error) {
       console.error("Error fetching exams:", error);
     }
@@ -163,6 +173,8 @@ const ExamCalendar = () => {
     }
   };
 
+
+
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
     <div style={{ width: "100%", textAlign: "center", paddingTop: "2%", padding: "10px", border: "1px solid #ccc", borderRadius: "5px", margin: "20px 0", boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)" }}>
@@ -230,8 +242,21 @@ const ExamCalendar = () => {
         </div>
       </div>
     </div>
+    <div style={{ width: "90%", }}>
+        <h2>Upcoming Exams</h2>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {examDates.map((examDate, index) => (
+             <li key={index} style={{ marginBottom: "10px", fontSize: "18px" }}>
+               <strong>Date:</strong> {examDate.getDate()}/{monthNames[examDate.getMonth()].substr(0, 3)}/{examDate.getFullYear().toString().substr(-2)}{" "} <span style={{ marginRight: "15px" }}></span>
+              <strong>Time:</strong> <span style={{ marginRight: "5px" }}></span>{examTimes[index]}{" "} <span style={{ marginRight: "15px" }}></span>
+              <strong>Exam Name:</strong> {examNames[index]}
+            </li>
+          ))}
+        </ul>
+      </div>
   </div>
 );
 };
+
 
 export default ExamCalendar;
